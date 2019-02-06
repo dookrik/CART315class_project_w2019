890a9988bec5042a2dd44b54635b210d83ab57af
Martin-John Hearty <martinjohn.hearty@gmail.com>
2019-02-06 14:53:17 -0500

    Set up for the Become scene + some test functionality of Become
    Set up a testing scene named Become. Added a script named Become that manges cameras on players and created a prefab camera.

---
+++ CART315ClassProjectWinter2019/Assets/SampleScenes/Scripts/Become.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Become : MonoBehaviour
+{
+    private Camera fpsCam;
+    private Ray ray;
+    public float clickRange = 100f;
+
+    void Start()
+    {
+        fpsCam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray rayEnd = fpsCam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(rayEnd, out hit, clickRange) && hit.collider.gameObject.GetComponent<CharacterController>() != null)
+            {
+                Become clickedObject = Instantiate(this, hit.collider.gameObject.transform.position, Quaternion.identity);
+                clickedObject.gameObject.name = "Camera_Become";
+                clickedObject.transform.SetParent(hit.collider.gameObject.transform);
+                clickedObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+                Destroy(gameObject);
+            }
+        }
+
+    }
+
+}
