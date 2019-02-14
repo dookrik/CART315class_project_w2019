c7bffacb998d31c31911d1dc5a9baaa977553966
Allan Pichardo <allan.pichardo@gmail.com>
2019-02-14 16:59:57 -0500

    Created utility function to calculate velocity vector given distance

---
+++ CART315ClassProjectWinter2019/Assets/Throw/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Projectile
+{
+    public const float GravityAcceleration = 9.8f;
+    
+    public static Vector3 GetProjectileVelocity(float initialVelocity, float distance, Vector3 up, Vector3 forward)
+    {
+        float g = Vector3.Magnitude(Physics.gravity * GravityAcceleration);
+        float theta = 0.5f * Mathf.Asin((g * distance) / (initialVelocity * initialVelocity));
+
+        Vector3 Vx = (initialVelocity * Mathf.Cos(theta)) * forward;
+        Vector3 Vy = (initialVelocity * Mathf.Sin(theta)) * up;
+
+        return Vx + Vy;
+    }
+
+}
