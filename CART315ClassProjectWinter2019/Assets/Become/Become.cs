2dd53a3b4be1596630d6f716edbf104dad656e81
Martin-John Hearty <martinjohn.hearty@gmail.com>
2019-02-15 21:34:04 -0500

    Becom Animation + locomotion sciprt
    Made progress on the camera animation while changing players
    
    enable and disable the LocomotionUserControl on players in the Become script

--- CART315ClassProjectWinter2019/Assets/Become/Become.cs
+++ CART315ClassProjectWinter2019/Assets/Become/Become.cs
@@ -7,12 +7,14 @@ public class Become : MonoBehaviour
     private Camera fpsCam;
     RaycastHit hit;
     private float clickRange = 100f;
+    private float t = 0.0f;
 
     void Start()
     {
         fpsCam = GetComponent<Camera>();
+        fpsCam.fieldOfView = 60;
     }
-    //we want to update every frame to make sure no frames are skipped because of UI input
+    //we want to update every frame
     void Update()
     {
         //if left click
@@ -22,21 +24,30 @@ public class Become : MonoBehaviour
             Ray rayEnd = fpsCam.ScreenPointToRay(Input.mousePosition);
 
             //if the mouse clicks on the gameobject and that gameobject has a CharacterController as a component, meaning that object is a player
-            if (Physics.Raycast(rayEnd, out hit, clickRange) && hit.collider.gameObject.GetComponent<CharacterController>() != null)
+            if (Physics.Raycast(rayEnd, out hit, clickRange) && hit.collider.gameObject.GetComponentInParent<CharacterController>() != null)
             {
-
-           
-                StartCoroutine("SwitchCameraDelay", 1.0f);
+                //add a delay for the camera-switch animation
+                StartCoroutine("SwitchCameraDelay", 0.7f);
             }
         }
-        //turn the camera towards the clicked object
-        if(hit.collider != null && hit.collider.gameObject.GetComponent<CharacterController>() != null)
+        //turn the camera towards the clicked object and make sure it's a player
+        if (hit.collider != null && hit.collider.gameObject.GetComponentInParent<CharacterController>() != null)
         {
             Vector3 direction = hit.collider.gameObject.transform.position - transform.position;
             Quaternion endRotation = Quaternion.LookRotation(direction);
             transform.rotation = Quaternion.Slerp(transform.rotation, endRotation, 20f * Time.deltaTime);
+            fpsCam.fieldOfView = Mathf.Lerp(60, 1, t);
         }
+        t += 0.7f * Time.deltaTime;
     }
+
+    IEnumerator SwitchCameraDelay(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        //code executes after the waitTime is elapsed 
+        SwitchCameras();
+    }
+   
 
     private void SwitchCameras()
     {
@@ -44,18 +55,22 @@ public class Become : MonoBehaviour
         Become clickedObject = Instantiate(this, hit.collider.gameObject.transform.position, Quaternion.identity);
         clickedObject.gameObject.name = "Camera_Become";
 
-        //make the camera a chid of the clicked game object to center its position relative to the player
+        //make the camera a chid of the clicked game object and center its position relative to the player
         clickedObject.transform.SetParent(hit.collider.gameObject.transform);
         clickedObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
 
+       // hit.collider.gameObject.GetComponentInChildren<Camera>().fieldOfView = Mathf.Lerp(20, 60, t);
+
+
+        //enable and disable the LocomotionUserControl - enable on one player at a time
+        if (hit.collider.gameObject.GetComponentInParent<LocomotionUserControl>() != null)
+        {
+            GetComponentInParent<LocomotionUserControl>().enabled = false;
+            hit.collider.gameObject.GetComponentInParent<LocomotionUserControl>().enabled = true;
+        }
+
         //Destroy the old camera.
         Destroy(gameObject);
-    }
-
-    IEnumerator SwitchCameraDelay(float waitTime)
-    {
-        yield return new WaitForSeconds(waitTime);
-        SwitchCameras();
     }
 
 }
