3861e13e7d6aa7cc5593b951a28f6b60b8032a72
ThomasGillard <38733378+ThomasGillard@users.noreply.github.com>
2019-02-16 13:42:38 -0500

    Updated Pickupper script to be compatible with throw

---
+++ CART315ClassProjectWinter2019/Assets/Throw.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+
+
+    //Check if IsHoldingObject()
+    //Check held object HeldObject()
+    //button fire and apply force Pickupper.Button Check
+    //
+
+    Pickupper pickupper = new Pickupper();
+    GameObject heldObject;
+    private bool toss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickupper = GetComponent<Pickupper>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (pickupper.IsHoldingObject())
+        {
+            heldObject = pickupper.HeldObject();
+            if (Input.GetButtonDown("Throw")){
+                pickupper.ButtonCheck();
+                //apply force here
+                print("throw");
+                heldObject = null;
+            }
+        }
+        else
+        {
+            heldObject = null;
+        }
+
+        
+    }
+
+
+}
