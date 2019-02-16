a19f095cf4cbd0ea776aebdad79ec1dc8661cdc8
Martin-John Hearty <martinjohn.hearty@gmail.com>
2019-02-15 19:04:58 -0500

    Become - look towards object
    The start of the camera animation (Become Script - testing)

--- CART315ClassProjectWinter2019/Assets/Become/Become.cs
+++ CART315ClassProjectWinter2019/Assets/Become/Become.cs
@@ -12,7 +12,7 @@ public class Become : MonoBehaviour
     {
         fpsCam = GetComponent<Camera>();
     }
-    //we want to update every frame to make sure no frames are skipped 
+    //we want to update every frame to make sure no frames are skipped because of UI input
     void Update()
     {
         //if left click
@@ -24,17 +24,38 @@ public class Become : MonoBehaviour
             //if the mouse clicks on the gameobject and that gameobject has a CharacterController as a component, meaning that object is a player
             if (Physics.Raycast(rayEnd, out hit, clickRange) && hit.collider.gameObject.GetComponent<CharacterController>() != null)
             {
-                //instantiate a new camera at the position of the object that was clicked on
-                Become clickedObject = Instantiate(this, hit.collider.gameObject.transform.position, Quaternion.identity);
-                //change the name of that camera to avoid (clone) being added every single time
-                clickedObject.gameObject.name = "Camera_Become";
-                //make the camera a chid of the clicked game object to center its position relative to the player
-                clickedObject.transform.SetParent(hit.collider.gameObject.transform);
-                clickedObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
-                //Destroy the old camera.
-                Destroy(gameObject);
+
+           
+                StartCoroutine("SwitchCameraDelay", 1.0f);
             }
         }
+        //turn the camera towards the clicked object
+        if(hit.collider != null && hit.collider.gameObject.GetComponent<CharacterController>() != null)
+        {
+            Vector3 direction = hit.collider.gameObject.transform.position - transform.position;
+            Quaternion endRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, endRotation, 20f * Time.deltaTime);
+        }
+    }
+
+    private void SwitchCameras()
+    {
+        //instantiate a new camera at the position of the object that was clicked on
+        Become clickedObject = Instantiate(this, hit.collider.gameObject.transform.position, Quaternion.identity);
+        clickedObject.gameObject.name = "Camera_Become";
+
+        //make the camera a chid of the clicked game object to center its position relative to the player
+        clickedObject.transform.SetParent(hit.collider.gameObject.transform);
+        clickedObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+        //Destroy the old camera.
+        Destroy(gameObject);
+    }
+
+    IEnumerator SwitchCameraDelay(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SwitchCameras();
     }
 
 }
