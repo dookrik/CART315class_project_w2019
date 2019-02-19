19bec44f91fe46cf59593776bb2c5d52a93a5673
Ebby <ebi.badavi@yahoo.com>
2019-02-18 23:13:24 -0500

    Eat Script Fundamentals
    * did Some adjusment on pickupper script
    * Player can eat everything it picks up :D

---
+++ CART315ClassProjectWinter2019/Assets/Eat/Eat.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eat : MonoBehaviour
+{
+    private Pickupper2 myFood;
+    Transform foodLoc;
+
+    void FixedUpdate()
+    {
+        
+        myFood = GameObject.Find("Player").GetComponent<Pickupper2>();
+
+        if (myFood.IsHoldingObject()) {
+            
+            foodLoc = myFood.grabPoint;
+
+            if (Input.GetButtonDown("Eat"))
+            {
+               
+
+                foreach (Transform child in foodLoc)
+                {
+                    if (child.gameObject != null)
+                    {
+                        //If the variables are public, we can do this:
+
+                        //myFood.isHolding = false;
+                        //myFood.buttonDown = false; 
+                        //myFood.pickup = null;
+
+                        // if No, I've created a function inside of Pickupper2 Script
+
+                        myFood.EatIt();
+                        Destroy(child.gameObject);
+                        CreateCube();
+                    }
+                }
+
+                
+
+                //Instantiate(food, foodLoc.position, Quaternion.identity);
+
+                //Debug.Log("fucked");
+            }
+        }
+    }
+
+    private void CreateCube()
+    {
+        GameObject food;
+        food = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        food.transform.localScale = foodLoc.localScale / 3;
+        food.transform.position = foodLoc.position;
+        food.AddComponent<Rigidbody>();
+        Destroy(food.gameObject, 2f);
+    }
+}
