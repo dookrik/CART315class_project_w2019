5e73d83d2033245b9654ab32bfe0c33a9749366f
geraldalvarez <gen.alv14@gmail.com>
2019-03-13 01:11:54 -0400

    Optimize the spawner script
    still need to fix the animation.

--- CART315ClassProjectWinter2019/Assets/Spawner.cs
+++ CART315ClassProjectWinter2019/Assets/Spawner.cs
@@ -8,130 +8,198 @@ public class Spawner : MonoBehaviour
     public GameObject[] spawnObject;
     //game object for the portal animation
     public GameObject portalAnimation;
+    //animation enable flag
+    public bool portalAnimationEnable = true;
     //audio clip for sound effect
     public AudioClip soundFx;
+    //max spawner
+    public int maxSpawn = 30;
+    //current number of spawned
+    private int currentSpawn = 0;
+    //enable the spawner interval
+    public bool spawnerIntervalEnable = true;
+    //spawner interval by seconds
+    public float spawnerInterval = 3f;
+    //z force physic
+    public float zForceThrust = 200;
+    //volume amplitude | 1f is max volume it could have
+    public float soundVolume = 1f;
+    //manual button for spawner
+    public string spawnerButton = "Fire1";
+    //manual button for iterating the array object
+    public string selectorButton = "Fire2";
 
     //array index indicator
     private int currentArrayIndex = 0;
-    //z force physic
-    private float zForceValue = 200;
     //audio source component. This component needs to be added on the object
     private AudioSource soundSource;
-    //volume amplitude
-    private float soundVolume = 1f;
-    //spawner interval by seconds
-    private float spawnerInterval = 3f;
     //spawner timer
     private float spawnerTimer;
     //reference to the portalAnimation prefab
     private GameObject portal;
     //portalAnimation's duration
-    private float portalAnimDur =1f;
+    private float portalAnimDur = 1f;
     //bool flag when to starting animating
     private bool startAnimPortal;
 
-  
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //check if there isn't a audioSource component
+        //check if there is an audio source
         if (GetComponent<AudioSource>() == null)
         {
-            //add an audioSource component
             soundSource = gameObject.AddComponent<AudioSource>() as AudioSource;
         }
         else
         {
-            //getting the audioSource Component
+            //initialize audiosource
             soundSource = GetComponent<AudioSource>();
         }
+
 
         //instantiating the portalAnimation
         portal = Instantiate(portalAnimation, transform.position, transform.rotation) as GameObject;
         portal.transform.localScale = new Vector3(1f, 1f, 1f);
-
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //add the deltaTime in the timer
-        spawnerTimer += Time.fixedDeltaTime;
 
-        //check if spawnerTimer exceeds the spawnerInterval
-        if (spawnerTimer > spawnerInterval)
-        {
-            //invoke spawn function
-            Spawn();
+        //invoke selectObject
+        selectObject(selectorButton);
 
-            //remove the interval in the timer
-            spawnerTimer -= spawnerInterval;
+        //invoke spawner with interval param
+        Spawn(spawnerInterval);
 
-            //setting the flag to true
-            startAnimPortal = true;
+        //invoke spawner with spawnerButton param
+        Spawn(spawnerButton);
 
-            //set the scale to the orginal
-            portal.transform.localScale = new Vector3(1f, 1f, 1f);
+        //invoke animation 
+        Animation(portalAnimationEnable);
 
-        }
-
-        //reset the portalAnimation to original scale
-        if (startAnimPortal)
-        {
-            
-           //scaling down the object
-            portal.transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
-          
-
-            //check if the animation duration is over
-            if (spawnerTimer > portalAnimDur)
-            {
-                //setting the flag to false
-                startAnimPortal = false;
-
-            }
-
-        }
-
-     
     }
 
-    //spawner function 
-    void Spawn()
+    //primary spawn function without parameter
+    public void Spawn()
     {
-        //play the sound fx        
-        soundSource.PlayOneShot(soundFx, soundVolume);
-
-        //create a temporary object holder for prefab cloning
-        GameObject tempObject = Instantiate(spawnObject[currentArrayIndex], transform.position, transform.rotation) as GameObject;       
-        //add relative force when the prefab is instantiated.
-        tempObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0,0, zForceValue));
-
-        //iterate the game object array
-        if (currentArrayIndex < spawnObject.Length - 1)
+        if (currentSpawn <= maxSpawn)
         {
-            currentArrayIndex++;
+            //play the sound fx        
+            soundSource.PlayOneShot(soundFx, soundVolume);
+
+            //create a temporary object holder for prefab cloning
+            GameObject tempObject = Instantiate(spawnObject[currentArrayIndex], transform.position, transform.rotation) as GameObject;
+            //add relative force when the prefab is instantiated.
+            tempObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, zForceThrust));
+
+            currentSpawn++;
         }
         else
         {
-            currentArrayIndex = 0;
+            Debug.Log("Exceed maxSpawn: " + currentSpawn);
+            //disable animation
+            portalAnimationEnable = false;
+            //disable interval
+            spawnerIntervalEnable = false;
+        }
+
+    }
+
+    //spawn function by interval
+    public void Spawn(float interval)
+    {
+
+        if (spawnerIntervalEnable)
+        {
+
+            //add the deltaTime in the timer
+            spawnerTimer += Time.fixedDeltaTime;
+
+
+            //check if spawnerTimer exceeds the spawnerInterval
+            if (spawnerTimer > interval)
+            {
+                //invoke spawn function
+                Spawn();
+
+                //remove the interval in the timer
+                spawnerTimer -= spawnerInterval;
+
+                //setting the flag to true
+                startAnimPortal = true;
+
+                //set the scale to the orginal
+                portal.transform.localScale = new Vector3(1f, 1f, 1f);
+
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    //spawn function by button click
+    public void Spawn(string button)
+    {
+        if (Input.GetButtonDown(button))
+        {
+            Debug.Log(button + " is pressed.");
+            //invoke spawn function
+            Spawn();
+        }
+    }
+
+    //animation function
+    public void Animation(bool animationEnable)
+    {
+        if (animationEnable)
+        {
+            //enable the display 
+            portal.GetComponent<MeshRenderer>().enabled = true;
+
+            if (startAnimPortal)
+            {
+                //scaling down the object
+                portal.transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
+
+                //check if the animation duration is over
+                if (spawnerTimer > portalAnimDur)
+                {
+                    //setting the flag to false
+                    startAnimPortal = false;
+                }
+            }
+        }
+        else
+        {
+            //set the scale to the orginal
+            portal.transform.localScale = new Vector3(1f, 1f, 1f);
+            //disable the display 
+            portal.GetComponent<MeshRenderer>().enabled = false;
         }
 
     }
 
     //selectObject selects which object to spawn
-    public void selectObject(int index)
+    public void selectObject(string selButton)
     {
-        //check if the index is within the range
-        if(index < spawnObject.Length - 1)
+        //if the button is pressed
+        if (Input.GetButtonDown(selButton))
         {
-            currentArrayIndex = index;
-        }
-        else
-        {
-            //console log error
-            Debug.Log("Index selected is out of bound.");
+            Debug.Log(selButton + " is pressed. " + currentArrayIndex);
+            //iterate the game object array
+            if (currentArrayIndex < spawnObject.Length - 1)
+            {
+                currentArrayIndex++;
+            }
+            else
+            {
+                currentArrayIndex = 0;
+            }
         }
     }
 }
