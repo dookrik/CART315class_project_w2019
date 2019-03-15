dcd8cb24ff05897d1f626c7297f00eb11e20c9cd
king kule <chortletortle@gmail.com>
2019-03-15 14:53:28 -0400

    New Locomotion using RigidBody

---
+++ CART315ClassProjectWinter2019/Assets/RigidBodyUser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyUser : MonoBehaviour
+{
+    private RigidBodyController charControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charControl = GetComponent<RigidBodyController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        charControl.Locomote(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+    }
+}
