564273d6f0f8f7c913eb32b9a50cd6a447e2533c
Martin-John Hearty <martinjohn.hearty@gmail.com>
2019-03-23 02:19:49 -0400

    Improved entrance to our island
    Added some images and tweaked some stuff on our team's island. Entrance still not complete.

--- CART315ClassProjectWinter2019/Assets/BubbleIslandTeam/scripts/BubbleEntrance.cs
+++ CART315ClassProjectWinter2019/Assets/BubbleIslandTeam/scripts/BubbleEntrance.cs
@@ -6,74 +6,116 @@ public class BubbleEntrance : MonoBehaviour
 {
     public Camera Cam;
     public GameObject MaskZone;
+    public GameObject innerDome;
     RaycastHit hit;
-
+    private int counter;
+    private Ray rayOrigin;
 
     private void Start()
     {
+        innerDome.SetActive(false);
         print((GetComponent<Renderer>().bounds.extents.x- (MaskZone.GetComponent<Renderer>().bounds.extents.z / 2))-0.2);
-
+        counter = 0;
     }
 
 
     private void FixedUpdate()
     {
+        rayOrigin = Cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        Debug.DrawRay(rayOrigin.origin, rayOrigin.direction * 10, Color.red);
+        if(counter == 2)
+        {
+            StartCoroutine(Delay(1));
+        }
+        if(counter > 2)
+        {
+            rayOrigin.direction = -rayOrigin.direction;
+
+        }
+    }
+
+    IEnumerator Delay(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        //code executes after the waitTime is elapsed 
+        innerDome.SetActive(true);
+        MaskZone.transform.localScale = new Vector3(0, 0, 0);
+        counter++;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Cam.transform.parent.gameObject)
+        {
+            print("enter");
+            counter++;
+        }
 
     }
 
     private void OnTriggerExit(Collider other)
     {
-        MaskZone.transform.localScale = new Vector3(0,0,5);
+        MaskZone.transform.localScale = new Vector3(0, 0, 0);
     }
+
     private void OnTriggerStay(Collider other)
     {
-        Ray rayOrigin = Cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
-        Debug.DrawRay(rayOrigin.origin, rayOrigin.direction * 10, Color.red);
-
         if (Physics.Raycast(rayOrigin, out hit, 10.0f))
         {
+            print(hit.collider.gameObject);
             if (hit.collider.gameObject.name == "Dome")
             {
-                //print(hit.distance);
+                OpenEntrance();
 
-                float x = 0;
-                float y = 0;
-                float z = 0;
-
-                if (x < 0.25 && y < 0.25 && z < 0.25)
-                {
-                    x = (1 / hit.distance) * 5;
-                    y = (1 / hit.distance) * 5;
-                    z = (1 / hit.distance) * 5;
-                }
-                else
-                {
-                   x = (1 / hit.distance) * 30;
-                   y = (1 / hit.distance) * 30;
-                   z = (1 / hit.distance) * 30;
-                }
-
-                if (x > 20 && z > 20)
-                {
-                    x = 10;
-                    z = 10;
-                }
-               
-                if (y > 0.35f)
-                {
-                    y = 0.35f;
-                }
-                if (hit.distance <= 0.15f)
-                {
-                    Cam.nearClipPlane = 0.8f;
-                }
-                else
-                {
-                    Cam.nearClipPlane = 0.2f;
-                }
-
-                MaskZone.transform.localScale = new Vector3(x, y, z);
+            }
+            if (hit.collider.gameObject.name == "InnerDome")
+            {
+               // OpenEntrance();
             }
         }
+    }
+
+    private void OpenEntrance()
+    {
+        print(1 / hit.distance);
+
+        float x = 0;
+        float y = 0;
+        float z = 0;
+
+        if (x < 0.25 && y < 0.25 && z < 0.25)
+        {
+            x = (1 / hit.distance) * 5;
+            y = (1 / hit.distance) * 5;
+            z = (1 / hit.distance) * 5;
+        }
+        else
+        {
+            x = (1 / hit.distance) * 30;
+            y = (1 / hit.distance) * 30;
+            z = (1 / hit.distance) * 30;
+        }
+
+        if (x > 20 && z > 20)
+        {
+            x = 10;
+            z = 10;
+        }
+
+        if (y > 0.35f)
+        {
+            y = 0.35f;
+        }
+        if (hit.distance <= 0.15f)
+        {
+            Cam.nearClipPlane = 0.8f;
+        }
+        else
+        {
+            Cam.nearClipPlane = 0.2f;
+        }
+
+        MaskZone.transform.localScale = new Vector3(x, y, z);
     }
 }
