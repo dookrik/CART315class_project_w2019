7a4d69c0e9c034cabd6c583715d5571008b385e1
Nina Parenteau <n_par@cda137ev5635x.local>
2019-04-03 14:00:27 -0400

    combustion script + buoyancy done
    Buoyancy: applies a counter gravity uplift based on if the object is submerged.
    Evaluates if the object is in contact with water
    
    combustion:
    -burns objects based on conductivity parameter, max temperatures
    -objects do not burn if submerged(buoyancy script reference)
    -objects burn if in contact with other burning objects
    -objects can burn eternally through eternal burn parameter.

--- CART315ClassProjectWinter2019/Assets/Combust/Combust.cs
+++ CART315ClassProjectWinter2019/Assets/Combust/Combust.cs
@@ -15,40 +15,55 @@ using UnityEngine;
 public class Combust : Break
 {
 
+    public bool eternalBurn = false;
     public float temperature;
+    public float maxTemperature;
+    public float conductiveness;
     public ParticleSystem particles; // Reference to particle system
 
     // burn function describes basic behaviour of a burning object
+    //public GameObject buoyancy = GameObject.getCom
+    public buoyancy buoyancy;
+
     public void burn()
     {
         // Where the flame particle system will be displayed
         Vector3 combustionPos = transform.position;
-
+        buoyancy = GetComponent<buoyancy>();
         GameObject particle = Instantiate(particles.gameObject);
         particle.transform.position = transform.position;
 
-        Destroy(particle, 6);
-
-        Invoke("explode", 5);
-
-        temperature += 10;
-
-        //When the temperature gets above 5, the object receives tag "Burning"
-        if(temperature > 5) 
+        if (buoyancy != null && (!buoyancy.isSubmerged || buoyancy == null))
         {
-            gameObject.tag = "Burning";
+            if (!eternalBurn && temperature >= maxTemperature)
+            {
+                Destroy(particle, 6);
+
+                Invoke("explode", 5);
+            }
+
+            if (temperature < maxTemperature)
+            {
+                    temperature += 1 * conductiveness;
+            }
+
+            //When the temperature gets above 5, the object receives tag "Burning"
+            if (temperature > 5)
+            {
+                gameObject.tag = "Burning";
+            }
         }
        
  
     }
 
     // Combustible object gets on fire on collision with a burning object
+    // the heat transfer is dependant of how conductive the object is
     public void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.CompareTag("Burning")) 
         {
-
-            this.temperature += 10;
+            this.temperature += 10*this.conductiveness;
             burn();
         }
 
--- CART315ClassProjectWinter2019/Assets/buoyancy.cs
+++ CART315ClassProjectWinter2019/Assets/buoyancy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class buoyancy : MonoBehaviour
 {
 
-    public Vector3 upLift = new Vector3(0, 14, 0);
+    public Vector3 upLift = new Vector3(0, 9.8f, 0);
     public Rigidbody rb;
     public float viscosity = 1f;
     public float drag = 1.5f;
@@ -31,12 +31,16 @@ public class buoyancy : MonoBehaviour
         colliderSize = collider.size;
         defaultCenter = collider.center;
 
+        /*
         firstLocalPoint = new Vector3(-colliderSize.x / 2, 0, colliderSize.z / 2);
         secondLocalPoint = new Vector3(-colliderSize.x / 2, 0, -colliderSize.z / 2);
         thirdLocalPoint = new Vector3(colliderSize.x / 2, 0, colliderSize.z / 2);
         fourthLocalPoint = new Vector3(colliderSize.x / 2, 0, -colliderSize.z / 2);
+        */       
 
         Vector3 waterPosition = GameObject.Find("Plane").transform.position;
+
+        collider.center = defaultCenter - new Vector3(0, colliderSize.y / 2, 0);
     }
 
     // Update is called once per frame
@@ -46,46 +50,54 @@ public class buoyancy : MonoBehaviour
         if (transform.position.y > waterPosition.y) isSubmerged = false;
         if (transform.position.y < waterPosition.y) isSubmerged = true;
 
-       collider.center = defaultCenter;
-        //int n = 0;
-        //firstWorldPoint = transform.TransformPoint(firstLocalPoint);
-        //secondWorldPoint = transform.TransformPoint(secondLocalPoint);
-        //thirdLocalPoint = transform.TransformPoint(thirdLocalPoint);
-        //fourthLocalPoint = transform.TransformPoint(fourthLocalPoint);
+        /*
+        //if ever you wanted to redefine the buoyancy based on an uplift that is placed at an
+        //approximation of the submerged center
+        int n = 0;
+        firstWorldPoint = transform.TransformPoint(firstLocalPoint);
+        secondWorldPoint = transform.TransformPoint(secondLocalPoint);
+        thirdLocalPoint = transform.TransformPoint(thirdLocalPoint);
+        fourthLocalPoint = transform.TransformPoint(fourthLocalPoint);
 
-        //if (firstWorldPoint.y <= waterPosition.y)
-        //{
-        //   collider.center += firstLocalPoint;
-        //    n++;
-        //}
-        //if (secondWorldPoint.y <= waterPosition.y)
-        //{
-        //    collider.center += secondLocalPoint;
-        //    n++;
-        //}
-        //if (thirdWorldPoint.y <= waterPosition.y)
-        //{
-        //   collider.center += thirdLocalPoint;
-        //    n++;
-        //}
-        //if (fourthWorldPoint.y <= waterPosition.y)
-        //{
-        //    collider.center += fourthLocalPoint;
-        //    n++;
-        //}
-       //collider.center = collider.center / n;
-        print(isSubmerged);
+        if (firstWorldPoint.y <= waterPosition.y)
+        {
+           collider.center += firstLocalPoint;
+            n++;
+        }
+        if (secondWorldPoint.y <= waterPosition.y)
+        {
+            collider.center += secondLocalPoint;
+            n++;
+        }
+        if (thirdWorldPoint.y <= waterPosition.y)
+        {
+           collider.center += thirdLocalPoint;
+            n++;
+        }
+        if (fourthWorldPoint.y <= waterPosition.y)
+        {
+            collider.center += fourthLocalPoint;
+            n++;
+        }
+        collider.center = collider.center / n;
+        */
+
+        //print(isSubmerged);
         if (isSubmerged)
         {
             rb.AddForce((rb.velocity * -drag * viscosity) + (upLift));
+
+            //if you wanted to add the force at the approximated center
             //rb.AddForceAtPosition((rb.velocity * -drag * viscosity) + upLift, vollider.center);
         }
+
+        //reset the center to default after each iteration
         //collider.center = defaultCenter;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        print("collision");
+        //print("collision");
         if (other.gameObject.layer == LayerMask.NameToLayer("Water"))
         {
             isSubmerged = true;
