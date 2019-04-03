392fbc4f2e0f46b3973d8b79b44aab5256b570d7
Cencu <31831899+Cencu@users.noreply.github.com>
2019-04-03 12:10:12 -0400

    Commit

---
+++ CART315ClassProjectWinter2019/Assets/RangeOpt/DisableIfFarAwayV2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableIfFarAwayV2 : MonoBehaviour
+{
+    private GameObject itemActivatorObject;
+    private ItemActivatorV2 activationScript;
+    public float distanceToDisable = 10;
+
+    // --------------------------------------------------
+
+    void Start()
+    {
+        itemActivatorObject = GameObject.Find("ItemActivatorObject");
+        activationScript = itemActivatorObject.GetComponent<ItemActivatorV2>();
+
+        StartCoroutine("AddToList");
+    }
+
+    IEnumerator AddToList()
+    {
+        yield return new WaitForSeconds(0.01f);
+       // Debug.Log(this.gameObject);
+
+        activationScript.addList.Add(new ActivatorItem { item = this.gameObject });
+    }
+}
+
+//HOW TO USE 
+//This script must be placed on every object in the game 
+//The input field allows you to choose the max distance you want to put for each object 
+
+//Credit goes to https://www.youtube.com/watch?v=tAbEvzZVz5E&feature=youtu.be as we used his script as the base
